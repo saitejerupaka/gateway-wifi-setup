6bdddb18c2ae8878e72f888dcafb4a56b195ccab
saitej.erupaka <saitej.erupaka@servicenow.com>
2018-11-29 12:32:15 -0800

    callback

--- index.js
+++ index.js
@@ -127,12 +127,13 @@ function callServiceNow(url, data, user, password){
     body:    JSON.stringify(data),
     'auth': auth
   };
-  request(options).then(function(response){
-      console.log(response.statusCode);
-  }).catch(function(error){
-    console.log(error);
+  request(options, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
     return;
   });
+
 }
 
 function handleSaitej(request, response, next){
